inhotel-io/inreveal

feat: do not offer filter sections that cannot filter anything (#910) (#935)
* docs: spec for interdependent filter sections (#910)

* docs: correct #910 spec after codebase review (rating >= semantics, facet-failure sentinel, {#key} invariant)

* docs: implementation plan for interdependent filter sections (#910)

Eight slice plans plus a spec correction: feedback_no_dynamic_rating_media_hiding
records a standing decision against per-star and per-button dimming, which the
first draft would have reversed. #910 is satisfied at section level instead.

* feat(server): add hasFavorites filter-suggestion facet (#910)

* docs: fold review findings into the #910 spec and all eight slice plans

Corrects three design-level defects and a set of plan defects found reviewing
the plans against the codebase:

- media availability was `mediaTypes.length < 2`, but the server returns raw
  distinct asset.type and AssetType includes AUDIO/OTHER, so a photo library
  with one OTHER asset passed with a dead Videos button. Now requires both
  IMAGE and VIDEO.
- the baseline was to be a second suggestionsProvider(createFilterState())
  call. The three query-mode pages keep a single smartFacetInFlight slot and
  abort on key mismatch, and their .then writes smartFacets, which feeds the
  timeline and the result count -- so that call would abort the real request
  and then render unfiltered data under active filters. Replaced with a
  config.baselineProvider hook that returns undefined in query mode.
- mobile had no baseline, so cross-section narrowing would pop sections in and
  out; it now shares the web model via the same family provider under
  SearchFilter.empty(). Its own null-response sentinel becomes a throw.

Also: e2e scope was three suites short (map seeds nothing; recently-added
asserts all ten sections) and People cannot be seeded without ML, so those
assertions invert; every pnpm test command dropped its path filter via a
stray `--`; make sql/open-api/lint-server and pnpm syn

e2e/src/specs/web/recently-added-filters.e2e-spec.ts
@@ -1,8 +1,9 @@
 import type { LoginResponseDto } from '@immich/sdk';
 import { updateAsset } from '@immich/sdk';
 import { expect, test } from '@playwright/test';
+import { readFileSync } from 'node:fs';
 import { thumbnailUtils } from 'src/ui/specs/timeline/utils';
-import { asBearerAuth, utils } from 'src/utils';
+import { asBearerAuth, testAssetDir, utils } from 'src/utils';
 
 test.describe('Recently Added', () => {
   let admin: LoginResponseDto;
@@ -84,16 +85,42 @@ test.describe('Recently Added filters', () => {
 
     // Seed with *taken* dates deliberately unrelated to upload order, so "ordered by added date"
     // is a meaningful assertion: taken dates run backwards while added order runs forwards.
+    // #910: two of the images carry real EXIF instead of synthetic bytes, so location/camera have
+    // something to populate their facets with — without real data those sections are correctly
+    // hidden. Swapping into the existing loop (not adding new assets) keeps TOTAL accurate for every
+    // header-count assertion below. See slice 6's recipe table.
     const images = [];
     for (let i = 0; i < TOTAL - VIDEOS; i++) {
       const day = String(TOTAL - VIDEOS - i).padStart(2, '0');
-      images.push(
-        await utils.createAsset(admin.accessToken, {
-          fileCreatedAt: `2023-09-${day}T10:00:00.000Z`,
-          fileModifiedAt: `2023-09-${day}T10:00:00.000Z`,
-        }),
-      );
+      const fileCreatedAt = `2023-09-${day}T10:00:00.000Z`;
+      const fileModifiedAt = fileCreatedAt;
+      if (i === 0) {
+        images.push(
+          await utils.createAsset(admin.accessToken, {
+            fileCreatedAt,
+            fileModifiedAt,
+            assetData: {
+              bytes: readFileSync(`${testAssetDir}/metadata/gps-position/thompson-springs.jpg`),
+              filename: 'gps.jpg',
+            },
+          }),
+        );
+      } else if (i === 1) {
+        images.push(
+          await utils.createAsset(admin.accessToken, {
+            fileCreatedAt,
+            fileModifiedAt,
+            assetData: {
+              bytes: readFileSync(`${testAssetDir}/metadata/rating/mongolels.jpg`),
+              filename: 'canon.jpg',
+            },
+          }),
+        );
+      } else {
+        images.push(await utils.createAsset(admin.accessToken, { fileCreatedAt, fileModifiedAt }));
+      }
     }
+    await utils.waitForQueueFinish(admin.accessToken, 'metadataExtraction');
 
     // Videos' taken dates run *opposite* to their upload order, so within the video-filtered set
     // "newest added first" and "newest taken first" disagree. That disagreement is the whole point
@@ -112,6 +139,26 @@ test.describe('Recently Added filters', () => {
     for (const asset of images.slice(0, RATED)) {
       await updateAsset({ id: asset.id, updateAssetDto: { rating: 5 } }, { headers: asBearerAuth(admin.accessToken) });
     }
+
+    // #910: tag/favorite/album metadata on already-seeded assets — no new assets, so TOTAL stays
+    // exact. See slice 6's recipe table.
+    const [tag] = await utils.upsertTags(admin.accessToken, ['e2e-recently-added-tag']);
+    await utils.tagAssets(admin.accessToken, tag.id, [images[2].id]);
+    await updateAsset(
+      { id: images[3].id, updateAssetDto: { isFavorite: true } },
+      { headers: asBearerAuth(admin.accessToken) },
+    );
+    // Albums needs BOTH sides — some filed, some not.
+    await utils.createAlbum(admin.accessToken, {
+      albumName: '#910 recently-added album',
+      assetIds: [images[4].id],
+    });
+
+    // People: createFace inserts asset_face + face_identity + face_identity_face directly, so it
+    // needs no ML — see e2e/src/utils.ts:490. No new asset, so TOTAL stays exact.
+    const person = await utils.createPerson(admin.accessToken, { name: '#910 Person' });
+    // sourceType: 'manual' — see utils.createFace; detection runs on these uploads.
+    await utils.createFace({ assetId: images[0].id, personId: person.id, sourceType: 'manual' });
   });
 
   async function gotoRecentlyAdded(
@@ -127,7 +174,7 @@ test.describe('Recently Added filters', () => {
     await page.waitForSelector('[data-testid="discovery-panel"], [data-testid="filter-toggle-btn"]');
   }
 
-  test('renders all ten metadata filter sections, including text', async ({ context, page }) => {
+  test('renders every metadata filter section its library can populate (#910)', async ({ context, page }) => {
     await gotoRecentlyAdded(context, page);
 
     await expect(page.getByTestId('discovery-panel')).toBeVisible();

e2e/src/utils.ts
@@ -509,7 +509,26 @@ export const utils = {
     return person;
   },
 
-  createFace: async ({ assetId, personId }: { assetId: string; personId: string }): Promise<string> => {
+  createFace: async ({
+    assetId,
+    personId,
+    sourceType = 'machine-learning',
+  }: {
+    assetId: string;
+    personId: string;
+    /**
+     * Defaults to the schema default, `machine-learning`, because that is what the face-repair
+     * surfaces require: getScanFlaggedFaces (and the cross-engine console) join `face_search` on
+     * `sourceType = MachineLearning`, so a face seeded any other way is invisible to them and the
+     * scan silently finds nothing — see face-cleanup / face-review-cross-engine.
+     *
+     * Pass `'manual'` from specs that upload real assets and let detection run: on a stack with
+     * facial recognition enabled, `handleDetectFaces` treats an existing MachineLearning-sourced
+     * face as a prior ML detection, re-runs detection, finds 0 real faces in these fixtures, and
+     * deletes every face whose id was not re-confirmed — wiping the seeded face.
+     */
+    sourceType?: 'machine-learning' | 'manual';
+  }): Promise<string> => {
     if (!client) {
       throw new Error('Database client not connected');
     }
@@ -517,8 +536,8 @@ export const utils = {
     const result = await client.query(
       `
       WITH inserted_face AS (
-        INSERT INTO asset_face ("assetId", "personId")
-        VALUES ($1, $2)
+        INSERT INTO asset_face ("assetId", "personId", "sourceType")
+        VALUES ($1, $2, $3)
         RETURNING id
       ),
       person_row AS (
@@ -552,7 +571,7 @@ export const utils = {
       SELECT (SELECT id FROM inserted_face), (SELECT id FROM resolved_identity), 'manual'
       RETURNING "assetFaceId" AS id
       `,
-      [assetId, personId],
+      [assetId, personId, sourceType],
     );
     return result.rows[0].id as string;
   },
@@ -625,9 +644,11 @@ export const utils = {
       );
       const globalPersonId = personResult.rows[0].id as string;
 
-      // 2. Create a face row linking the asset to the global person.
+      // 2. Create a face row linking the asset to the global person. sourceType: 'manual' — see the
+      // comment on utils.createFace above; left at the default it gets silently deleted by
+      // handleDetectFaces on any stack with facial recognition enabled.
       const faceResult = await client.query(
-        `INSERT INTO "asset_face" ("assetId", "personId") VALUES ($1, $2) RETURNING id`,
+        `INSERT INTO "asset_face" ("assetId", "personId", "sourceType") VALUES ($1, $2, 'manual') RETURNING id`,
         [assetId, globalPersonId],
       );
       const faceId = faceResult.rows[0].id as string;

alangrainger/immich

e2e/src/specs/web/recently-added-filters.e2e-spec.ts
@@ -1,8 +1,9 @@
 import type { LoginResponseDto } from '@immich/sdk';
 import { updateAsset } from '@immich/sdk';
 import { expect, test } from '@playwright/test';
+import { readFileSync } from 'node:fs';
 import { thumbnailUtils } from 'src/ui/specs/timeline/utils';
-import { asBearerAuth, utils } from 'src/utils';
+import { asBearerAuth, testAssetDir, utils } from 'src/utils';
 
 test.describe('Recently Added', () => {
   let admin: LoginResponseDto;
@@ -84,16 +85,42 @@ test.describe('Recently Added filters', () => {
 
     // Seed with *taken* dates deliberately unrelated to upload order, so "ordered by added date"
     // is a meaningful assertion: taken dates run backwards while added order runs forwards.
+    // #910: two of the images carry real EXIF instead of synthetic bytes, so location/camera have
+    // something to populate their facets with — without real data those sections are correctly
+    // hidden. Swapping into the existing loop (not adding new assets) keeps TOTAL accurate for every
+    // header-count assertion below. See slice 6's recipe table.
     const images = [];
     for (let i = 0; i < TOTAL - VIDEOS; i++) {
       const day = String(TOTAL - VIDEOS - i).padStart(2, '0');
-      images.push(
-        await utils.createAsset(admin.accessToken, {
-          fileCreatedAt: `2023-09-${day}T10:00:00.000Z`,
-          fileModifiedAt: `2023-09-${day}T10:00:00.000Z`,
-        }),
-      );
+      const fileCreatedAt = `2023-09-${day}T10:00:00.000Z`;
+      const fileModifiedAt = fileCreatedAt;
+      if (i === 0) {
+        images.push(
+          await utils.createAsset(admin.accessToken, {
+            fileCreatedAt,
+            fileModifiedAt,
+            assetData: {
+              bytes: readFileSync(`${testAssetDir}/metadata/gps-position/thompson-springs.jpg`),
+              filename: 'gps.jpg',
+            },
+          }),
+        );
+      } else if (i === 1) {
+        images.push(
+          await utils.createAsset(admin.accessToken, {
+            fileCreatedAt,
+            fileModifiedAt,
+            assetData: {
+              bytes: readFileSync(`${testAssetDir}/metadata/rating/mongolels.jpg`),
+              filename: 'canon.jpg',
+            },
+          }),
+        );
+      } else {
+        images.push(await utils.createAsset(admin.accessToken, { fileCreatedAt, fileModifiedAt }));
+      }
     }
+    await utils.waitForQueueFinish(admin.accessToken, 'metadataExtraction');
 
     // Videos' taken dates run *opposite* to their upload order, so within the video-filtered set
     // "newest added first" and "newest taken first" disagree. That disagreement is the whole point
@@ -112,6 +139,26 @@ test.describe('Recently Added filters', () => {
     for (const asset of images.slice(0, RATED)) {
       await updateAsset({ id: asset.id, updateAssetDto: { rating: 5 } }, { headers: asBearerAuth(admin.accessToken) });
     }
+
+    // #910: tag/favorite/album metadata on already-seeded assets — no new assets, so TOTAL stays
+    // exact. See slice 6's recipe table.
+    const [tag] = await utils.upsertTags(admin.accessToken, ['e2e-recently-added-tag']);
+    await utils.tagAssets(admin.accessToken, tag.id, [images[2].id]);
+    await updateAsset(
+      { id: images[3].id, updateAssetDto: { isFavorite: true } },
+      { headers: asBearerAuth(admin.accessToken) },
+    );
+    // Albums needs BOTH sides — some filed, some not.
+    await utils.createAlbum(admin.accessToken, {
+      albumName: '#910 recently-added album',
+      assetIds: [images[4].id],
+    });
+
+    // People: createFace inserts asset_face + face_identity + face_identity_face directly, so it
+    // needs no ML — see e2e/src/utils.ts:490. No new asset, so TOTAL stays exact.
+    const person = await utils.createPerson(admin.accessToken, { name: '#910 Person' });
+    // sourceType: 'manual' — see utils.createFace; detection runs on these uploads.
+    await utils.createFace({ assetId: images[0].id, personGroupId: person.id, sourceType: 'manual' });
   });
 
   async function gotoRecentlyAdded(
@@ -127,7 +174,7 @@ test.describe('Recently Added filters', () => {
     await page.waitForSelector('[data-testid="discovery-panel"], [data-testid="filter-toggle-btn"]');
   }
 
-  test('renders all ten metadata filter sections, including text', async ({ context, page }) => {
+  test('renders every metadata filter section its library can populate (#910)', async ({ context, page }) => {
     await gotoRecentlyAdded(context, page);
 
     await expect(page.getByTestId('discovery-panel')).toBeVisible();

e2e/src/utils.ts
@@ -534,7 +534,26 @@ export const utils = {
     return person;
   },
 
-  createFace: async ({ assetId, personGroupId }: { assetId: string; personGroupId: string }): Promise<string> => {
+  createFace: async ({
+    assetId,
+    personGroupId,
+    sourceType = 'machine-learning',
+  }: {
+    assetId: string;
+    personGroupId: string;
+    /**
+     * Defaults to the schema default, `machine-learning`, because that is what the face-repair
+     * surfaces require: getScanFlaggedFaces (and the cross-engine console) join `face_search` on
+     * `sourceType = MachineLearning`, so a face seeded any other way is invisible to them and the
+     * scan silently finds nothing — see face-cleanup / face-review-cross-engine.
+     *
+     * Pass `'manual'` from specs that upload real assets and let detection run: on a stack with
+     * facial recognition enabled, `handleDetectFaces` treats an existing MachineLearning-sourced
+     * face as a prior ML detection, re-runs detection, finds 0 real faces in these fixtures, and
+     * deletes every face whose id was not re-confirmed — wiping the seeded face.
+     */
+    sourceType?: 'machine-learning' | 'manual';
+  }): Promise<string> => {
     if (!client) {
       throw new Error('Database client not connected');
     }
@@ -542,8 +561,8 @@ export const utils = {
     const result = await client.query(
       `
       WITH inserted_face AS (
-        INSERT INTO asset_face ("assetId", "personGroupId")
-        VALUES ($1, $2)
+        INSERT INTO asset_face ("assetId", "personGroupId", "sourceType")
+        VALUES ($1, $2, $3)
         RETURNING id
       ),
       person_row AS (
@@ -577,7 +596,7 @@ export const utils = {
       SELECT (SELECT id FROM inserted_face), (SELECT id FROM resolved_identity), 'manual'
       RETURNING "assetFaceId" AS id
       `,
-      [assetId, personGroupId],
+      [assetId, personGroupId, sourceType],
     );
     return result.rows[0].id as string;
   },
@@ -663,9 +682,11 @@ export const utils = {
       );
       const globalPersonId = personResult.rows[0].id as string;
 
-      // 2. Create a face row linking the asset to the global person.
+      // 2. Create a face row linking the asset to the global person. sourceType: 'manual' — see the
+      // comment on utils.createFace above; left at the default it gets silently deleted by
+      // handleDetectFaces on any stack with facial recognition enabled.
       const faceResult = await client.query(
-        `INSERT INTO "asset_face" ("assetId", "personGroupId") VALUES ($1, $2) RETURNING id`,
+        `INSERT INTO "asset_face" ("assetId", "personGroupId", "sourceType") VALUES ($1, $2, 'manual') RETURNING id`,
         [assetId, globalPersonId],
       );
       const faceId = faceResult.rows[0].id as string;