BierDav/noodle-gallery

fix(mobile): start each search at Relevance and hide memories while filtering (#902) (#906)
* fix(mobile): start each search at Relevance and hide memories while filtering (#902)

Two Photos-tab search behaviours diverged from web.

Sort order: `setText` rewrote a stored Relevance sort into Newest whenever the
query emptied. Nothing ever wrote Relevance back, so the first time a user
cleared the search box the app was pinned to "Newest first" for every later
search. Relevance already degrades to newest-first where it has to (no `order`
on a metadata search request, date grouping in the timeline, "Newest first" in
the sort sheet), so the stored value never needed coercing. It is now reset to
Relevance whenever the query itself changes — the same contract web's search
palette has, where `searchSortOrder` returns to relevance as a session opens on
an empty query and when the query is cleared. A chosen sort applies to the
results in front of the user and is dropped once they search for something else;
re-submitting the same text is not a new search and keeps it.

Memories strip: web's photos page mounts the carousel only while nothing is
being searched or filtered (`!hasActiveFilters`), so results start at the top of
the viewport. Mobile kept it pinned above search results. `photosMemoryLaneVisibleProvider`
now gates both the sliver and the scroll offset reserved for it.

The result layout the report also mentions — a compact grid for Relevance,
month/day headers for the date orders — already matches web (`space-search-results.svelte`
groups by month unless `sortMode === 'relevance'`), and Relevance being the
default again means searches open in the compact grid.

* fix(mobile): point the scroll-drain grouping switch at the overview mode

#911 renamed `timelineGroupingProvider` to `timelineOverviewModeProvider` and
reshaped `TimelineGroupingSpec` / `TimelineOverviewSegment`. #886 was written
against the previous names and merged after it, so main no longer compiles —
`S

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -12,11 +12,15 @@ import 'package:immich_mobile/presentation/widgets/timeline/timeline_empty_state
 import 'package:immich_mobile/presentation/widgets/timeline/timeline_grouping_selector.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline_route_scope.dart';
 import 'package:immich_mobile/providers/feature_message.provider.dart';
-import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/memory_lane.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/photos_filter_search.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
+
+/// Scroll offset the memories strip occupies at the top of the timeline —
+/// matches [DriftMemoryLane]'s own max height so the scrubber lines up.
+const double _memoryLaneHeight = 200;
 
 @RoutePage()
 class MainTimelinePage extends ConsumerStatefulWidget {
@@ -52,7 +56,9 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
 
   @override
   Widget build(BuildContext context) {
-    final hasMemories = ref.watch(driftMemoryFutureProvider.select((state) => state.value?.isNotEmpty ?? false));
+    // The memories strip is browse-only: an active search or filter takes the
+    // space back so results start at the top of the viewport (#902, web parity).
+    final showMemories = ref.watch(photosMemoryLaneVisibleProvider);
     return TimelineRouteScope(
       timelineServiceBuilder: buildPhotosTimelineRouteService,
       // The main Photos timeline is the only route on the app-level grouping, so its
@@ -77,13 +83,13 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
                 return false;
               },
               child: Timeline(
-                topSliverWidget: const SliverMainAxisGroup(
+                topSliverWidget: SliverMainAxisGroup(
                   slivers: [
-                    PhotosFilterSubheader(),
-                    SliverToBoxAdapter(child: DriftMemoryLane()),
+                    const PhotosFilterSubheader(),
+                    if (showMemories) const SliverToBoxAdapter(child: DriftMemoryLane()),
                   ],
                 ),
-                topSliverWidgetHeight: hasMemories ? 200 : 0,
+                topSliverWidgetHeight: showMemories ? _memoryLaneHeight : 0,
                 showStorageIndicator: true,
                 appBar: const PhotosTimelineAppBar(),
                 bottomSliverWidget: const _SearchLoadMoreFooter(),

alangrainger/immich

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -13,11 +13,15 @@ import 'package:immich_mobile/presentation/widgets/timeline/timeline_empty_state
 import 'package:immich_mobile/presentation/widgets/timeline/timeline_grouping_selector.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline_route_scope.dart';
 import 'package:immich_mobile/providers/feature_message.provider.dart';
-import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/memory_lane.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/photos_filter_search.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
+
+/// Scroll offset the memories strip occupies at the top of the timeline —
+/// matches [DriftMemoryLane]'s own max height so the scrubber lines up.
+const double _memoryLaneHeight = 200;
 
 @RoutePage()
 class MainTimelinePage extends ConsumerStatefulWidget {
@@ -53,7 +57,9 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
 
   @override
   Widget build(BuildContext context) {
-    final hasMemories = ref.watch(memoryLaneProvider.select((state) => state.value?.isNotEmpty ?? false));
+    // The memories strip is browse-only: an active search or filter takes the
+    // space back so results start at the top of the viewport (#902, web parity).
+    final showMemories = ref.watch(photosMemoryLaneVisibleProvider);
     return TimelineRouteScope(
       timelineServiceBuilder: buildPhotosTimelineRouteService,
       // The main Photos timeline is the only route on the app-level grouping, so its
@@ -78,13 +84,13 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
                 return false;
               },
               child: Timeline(
-                topSliverWidget: const SliverMainAxisGroup(
+                topSliverWidget: SliverMainAxisGroup(
                   slivers: [
-                    PhotosFilterSubheader(),
-                    SliverToBoxAdapter(child: MemoryLane()),
+                    const PhotosFilterSubheader(),
+                    if (showMemories) const SliverToBoxAdapter(child: MemoryLane()),
                   ],
                 ),
-                topSliverWidgetHeight: hasMemories ? 200 : 0,
+                topSliverWidgetHeight: showMemories ? _memoryLaneHeight : 0,
                 showStorageIndicator: true,
                 appBar: const PhotosTimelineAppBar(),
                 bottomSliverWidget: const _SearchLoadMoreFooter(),