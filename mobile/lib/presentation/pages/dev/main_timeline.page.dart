inhotel-io/inreveal

docs(mobile): design spec + mockup for new bottom nav (#378)
* docs(mobile): design spec + mockup for new bottom nav

New floating-pill bottom nav (Photos · Albums · Library) plus an
outboard Search blob that routes to Photos + opens the FilterSheet.
Retires the Search and Spaces tabs from the bottom nav; existing
routes (DriftSearchRoute, SpacesRoute) stay reachable.

Scope
- Photos / Albums (DriftAlbumsRoute) / Library (DriftLibraryRoute)
- Outboard Search blob → openGallerySearch(context, ref)
- New fork-only GalleryTabShellPage; upstream tab_shell.page.dart
  stays bit-identical
- Aesthetic "Darkroom Warmth" — mockup shows amber demo palette;
  Flutter reads theme.colorScheme.primary in production

Out of scope (follow-ups)
- Spaces surfacing inside DriftLibraryPage
- Any change to the Albums page itself
- Backstack-migration shims (auto_route default-route fallback covers)

Mockup: docs/plans/mockups/2026-04-18-mobile-bottom-nav.html

* docs(mobile): bottom-nav design rev 2 — fold in review findings

Addresses the /review pass against rev 1:

Blockers
- Fork-only galleryTabProvider + GalleryTabEnum; upstream
  tab.provider.dart / tabProvider stay untouched (was silently
  conflating indices with upstream TabEnum).
- Focus plumbing for the FilterSheet search input: adds a FocusNode
  to FilterSheetSearchBar + a counter-based request provider
  (photosFilterSearchFocusRequestProvider). Rev 1 claimed the node
  existed; it didn't.
- openGallerySearch now waits 620 ms to cover upstream's 600 ms
  FadeTransition before writing the sheet state / requesting focus
  (was endOfFrame — ~16 ms — producing visible stutter + dropped
  focus requests).

High
- §8.2 side-effect matrix now covers every row in §6.5 plus negative
  assertions (sharedSpacesProvider / searchPreFilterProvider /
  searchInputFocusProvider / upstream tabProvider are NOT touched).
- Keyboard-hide threshold unified to 80 pt absolute (was 20 % of
  viewport in §5.3 vs 400 pt in §8.2).
- Peek-rail coexis

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -4,11 +4,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_sheet.widget.dart';
 import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart';
+import 'package:immich_mobile/presentation/widgets/photos_filter/filter_subheader.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
-import 'package:immich_mobile/providers/photos_filter/filter_sheet.provider.dart';
-import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
 
@@ -23,25 +22,18 @@ class MainTimelinePage extends ConsumerStatefulWidget {
 class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
   @override
   Widget build(BuildContext context) {
-    // Auto-peek when the first filter is added from a hidden sheet; auto-collapse
-    // when the last chip is removed while peek is showing (design §6.2).
-    ref.listen<bool>(photosFilterProvider.select((f) => f.isEmpty), (prev, next) {
-      if (prev == next) return;
-      final sheet = ref.read(photosFilterSheetProvider);
-      if (prev == true && next == false && sheet == FilterSheetSnap.hidden) {
-        ref.read(photosFilterSheetProvider.notifier).state = FilterSheetSnap.peek;
-      } else if (prev == false && next == true && sheet == FilterSheetSnap.peek) {
-        ref.read(photosFilterSheetProvider.notifier).state = FilterSheetSnap.hidden;
-      }
-    });
-
     final hasMemories = ref.watch(driftMemoryFutureProvider.select((state) => state.value?.isNotEmpty ?? false));
     return ProviderScope(
       overrides: [timelineServiceProvider.overrideWith((ref) => ref.watch(photosTimelineQueryProvider))],
       child: Stack(
         children: [
           Timeline(
-            topSliverWidget: const SliverToBoxAdapter(child: DriftMemoryLane()),
+            topSliverWidget: const SliverMainAxisGroup(
+              slivers: [
+                PhotosFilterSubheader(),
+                SliverToBoxAdapter(child: DriftMemoryLane()),
+              ],
+            ),
             topSliverWidgetHeight: hasMemories ? 200 : 0,
             showStorageIndicator: true,
             appBar: const ImmichSliverAppBar(floating: true, pinned: false, snap: false, actions: [FilterIconButton()]),

alangrainger/immich

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -5,12 +5,11 @@ import 'package:immich_mobile/presentation/widgets/feature_message/feature_messa
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_sheet.widget.dart';
 import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart';
+import 'package:immich_mobile/presentation/widgets/photos_filter/filter_subheader.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
 import 'package:immich_mobile/providers/feature_message.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
-import 'package:immich_mobile/providers/photos_filter/filter_sheet.provider.dart';
-import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
 
@@ -46,25 +45,18 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
 
   @override
   Widget build(BuildContext context) {
-    // Auto-peek when the first filter is added from a hidden sheet; auto-collapse
-    // when the last chip is removed while peek is showing (design §6.2).
-    ref.listen<bool>(photosFilterProvider.select((f) => f.isEmpty), (prev, next) {
-      if (prev == next) return;
-      final sheet = ref.read(photosFilterSheetProvider);
-      if (prev == true && next == false && sheet == FilterSheetSnap.hidden) {
-        ref.read(photosFilterSheetProvider.notifier).state = FilterSheetSnap.peek;
-      } else if (prev == false && next == true && sheet == FilterSheetSnap.peek) {
-        ref.read(photosFilterSheetProvider.notifier).state = FilterSheetSnap.hidden;
-      }
-    });
-
     final hasMemories = ref.watch(memoryLaneProvider.select((state) => state.value?.isNotEmpty ?? false));
     return ProviderScope(
       overrides: [timelineServiceProvider.overrideWith((ref) => ref.watch(photosTimelineQueryProvider))],
       child: Stack(
         children: [
           Timeline(
-            topSliverWidget: const SliverToBoxAdapter(child: MemoryLane()),
+            topSliverWidget: const SliverMainAxisGroup(
+              slivers: [
+                PhotosFilterSubheader(),
+                SliverToBoxAdapter(child: MemoryLane()),
+              ],
+            ),
             topSliverWidgetHeight: hasMemories ? 200 : 0,
             showStorageIndicator: true,
             appBar: const ImmichSliverAppBar(floating: true, pinned: false, snap: false, actions: [FilterIconButton()]),