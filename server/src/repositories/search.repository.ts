inhotel-io/inreveal

feat: search result sorting in shared spaces (#254)
* docs: search sorting design for space search

Two-phase CTE approach: recall top-500 by vector similarity,
re-sort by date. Sort dropdown (relevance/newest/oldest),
infinite scroll pagination, date-grouped display.

* docs: address review findings in search sorting design

- Explicit clearSearch/clearFilters sort behavior distinction
- Type narrowing for SortToggle with safety expression
- Drive-by: isFavorite mapping + $effect dep (pre-existing gaps)
- Extended test plan: handleSearchSubmit, clearSearch, sort switch,
  timeline fallback tests

* docs: add clearFilters preserves relevance test to design

* docs: search sorting implementation plan

13-task TDD plan covering backend CTE, FilterState extension,
sort dropdown, infinite scroll, date grouping, and codegen.

* docs: address review recommendations in implementation plan

* feat: add order param to SmartSearchDto

* feat: pass order through to smart search repository

* feat: two-phase CTE for date-sorted smart search

* feat: extend FilterState.sortOrder with 'relevance'

* feat: pass sort order and isFavorite in space search params

* feat: space page sort entry/exit/reset behavior

* feat: search sort dropdown component

* feat: wire sort dropdown into space search UI

* feat: infinite scroll, date grouping, and contextual result count

* feat: pass sortMode to SpaceSearchResults

* chore: regenerate OpenAPI specs, SDK, and SQL queries

* fix: review fixes — observer cleanup, design tokens, type safety

- Add cleanup return to IntersectionObserver $effect to prevent leaks
- Replace hardcoded colors with bg-light/hover:bg-subtle design tokens
- Replace `as any` with `as MapAsset[]` for proper type safety
- Add safety comment explaining sql.raw(orderDirection) is enum-validated
- Use explicit en-US locale in date grouping for CI stability

* fix: ESLint unicorn rules — ternaries and negated condition

* fix: update SQL query file for CTE searchSmart

* f

server/src/repositories/search.repository.ts
@@ -143,7 +143,8 @@ export type SmartSearchOptions = SearchDateOptions &
   SearchPeopleOptions &
   SearchTagOptions &
   SearchOcrOptions &
-  SearchSpaceOptions;
+  SearchSpaceOptions &
+  SearchOrderOptions;
 
 export type OcrSearchOptions = SearchDateOptions & SearchOcrOptions;
 
@@ -337,6 +338,7 @@ export class SearchRepository {
         isFavorite: true,
         userIds: [DummyValue.UUID],
         spacePersonIds: [DummyValue.UUID],
+        orderDirection: 'desc',
       },
     ],
   })
@@ -347,10 +349,27 @@ export class SearchRepository {
 
     return this.db.transaction().execute(async (trx) => {
       await sql`set local vchordrq.probes = ${sql.lit(probes[VectorIndex.Clip])}`.execute(trx);
-      const items = await searchAssetBuilder(trx, options)
+
+      const baseQuery = searchAssetBuilder(trx, options)
         .selectAll('asset')
         .innerJoin('smart_search', 'asset.id', 'smart_search.assetId')
-        .orderBy(sql`smart_search.embedding <=> ${options.embedding}`)
+        .orderBy(sql`smart_search.embedding <=> ${options.embedding}`);
+
+      if (options.orderDirection) {
+        const orderDirection = options.orderDirection.toLowerCase() as OrderByDirection;
+        const candidates = baseQuery.limit(500).as('candidates');
+        const items = await trx
+          .selectFrom(candidates)
+          .selectAll()
+          // sql.raw is safe here — orderDirection is validated to 'asc'|'desc' by the AssetOrder enum
+          .orderBy(sql`"candidates"."fileCreatedAt" ${sql.raw(orderDirection)} nulls last`)
+          .limit(pagination.size + 1)
+          .offset((pagination.page - 1) * pagination.size)
+          .execute();
+        return paginationHelper(items as MapAsset[], pagination.size);
+      }
+
+      const items = await baseQuery
         .limit(pagination.size + 1)
         .offset((pagination.page - 1) * pagination.size)
         .execute();

alangrainger/immich

server/src/repositories/search.repository.ts
@@ -172,7 +172,8 @@ export type SmartSearchOptions = SearchDateOptions &
   SearchPeopleOptions &
   SearchTagOptions &
   SearchOcrOptions &
-  SearchSpaceOptions & { visibility?: AssetVisibility | 'not-locked'; viewingUserId?: string };
+  SearchSpaceOptions &
+  SearchOrderOptions & { visibility?: AssetVisibility | 'not-locked'; viewingUserId?: string };
 
 export type LargeAssetSearchOptions = AssetSearchOptions & { minFileSize?: number };
 
@@ -362,6 +363,7 @@ export class SearchRepository {
         isFavorite: true,
         userIds: [DummyValue.UUID],
         spacePersonIds: [DummyValue.UUID],
+        orderDirection: 'desc',
       },
     ],
   })
@@ -372,11 +374,27 @@ export class SearchRepository {
 
     return this.db.transaction().execute(async (trx) => {
       await sql`set local vchordrq.probes = ${sql.lit(probes[VectorIndex.Clip])}`.execute(trx);
-      const items = await searchAssetBuilderLegacy(trx, options)
+      const baseQuery = searchAssetBuilderLegacy(trx, options)
         .select(columns.searchAsset)
         .innerJoin('smart_search', 'asset.id', 'smart_search.assetId')
         .orderBy(sql`smart_search.embedding <=> ${options.embedding}`)
-        .orderBy('asset.id', 'asc')
+        .orderBy('asset.id', 'asc');
+
+      if (options.orderDirection) {
+        const orderDirection = options.orderDirection.toLowerCase() as OrderByDirection;
+        const candidates = baseQuery.limit(500).as('candidates');
+        const items = await trx
+          .selectFrom(candidates)
+          .selectAll()
+          // sql.raw is safe here — orderDirection is validated to 'asc'|'desc' by the AssetOrder enum
+          .orderBy(sql`"candidates"."fileCreatedAt" ${sql.raw(orderDirection)} nulls last`)
+          .limit(pagination.size + 1)
+          .offset((pagination.page - 1) * pagination.size)
+          .execute();
+        return paginationHelper(items as MapAsset[], pagination.size);
+      }
+
+      const items = await baseQuery
         .limit(pagination.size + 1)
         .offset((pagination.page - 1) * pagination.size)
         .execute();