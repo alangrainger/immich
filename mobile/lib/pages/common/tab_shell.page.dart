inhotel-io/inreveal

feat(mobile): promote Spaces to bottom nav with card redesign (#116)
* docs: add demo mode design document

Design for a read-only demo mode that lets visitors experience
the full Gallery UI via a "Try Demo" button on the login page.
Env-var driven, server-side write guard, client-side UI hiding.

* docs: add demo mode implementation plan

* feat(demo): add IMMICH_DEMO_MODE env vars to EnvDto and EnvData

* feat(demo): expose demoMode in server config endpoint

* feat(demo): add POST /auth/demo-login endpoint

* feat(demo): add DemoInterceptor to block writes for demo user

* chore: regenerate OpenAPI SDK with demo mode endpoints

* feat(demo): add Try Demo button to login page

* feat(demo): hide write actions in UI for demo user

* fix(demo): address lint and type check issues in test files

* style: format demo mode test file

* fix(demo): allow change-password through demo interceptor

* fix(demo): allow onboarding and preferences PUT endpoints through demo interceptor

* fix(demo): hide create space button for demo user

* feat(web): add umami analytics tracking to demo

Adds the umami tracking script pointing to analytics.opennoodle.de
with website ID 25843121-9ac1-4f07-877b-72f55e6e9de0.

* fix(demo): allow sync endpoints through demo interceptor

* fix(demo): allow all sync endpoints through demo interceptor

* docs: add design for promoting spaces to mobile bottom nav

Replaces Albums tab with Spaces, moves Albums into Library,
and redesigns space cards with collage thumbnails matching web.

* docs: add implementation plan for mobile spaces bottom nav

* refactor(mobile): rename album tab constants and enum to spaces

Rename kAlbumTabIndex to kSpacesTabIndex, TabEnum.albums to
TabEnum.spaces, and update tab_shell and app_life_cycle providers
to reflect the new Spaces tab replacing Albums in the bottom nav.

* feat(mobile): swap Albums tab for Spaces in bottom navigation

Replace the Albums NavigationDestination with Spaces (workspaces icon)
in both legacy T

mobile/lib/pages/common/tab_shell.page.dart
@@ -11,6 +11,7 @@ import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/presentation/pages/search/paginated_search.provider.dart';
 import 'package:immich_mobile/providers/haptic_feedback.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
+import 'package:immich_mobile/providers/shared_space.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/readonly_mode.provider.dart';
@@ -45,9 +46,9 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
         enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
-        label: 'albums'.tr(),
-        icon: const Icon(Icons.photo_album_outlined),
-        selectedIcon: Icon(Icons.photo_album_rounded, color: context.primaryColor),
+        label: 'Spaces',
+        icon: const Icon(Icons.workspaces_outlined),
+        selectedIcon: Icon(Icons.workspaces, color: context.primaryColor),
         enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
@@ -78,7 +79,7 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
     }
 
     return AutoTabsRouter(
-      routes: const [MainTimelineRoute(), DriftSearchRoute(), DriftAlbumsRoute(), DriftLibraryRoute()],
+      routes: const [MainTimelineRoute(), DriftSearchRoute(), SpacesRoute(), DriftLibraryRoute()],
       duration: const Duration(milliseconds: 600),
       transitionBuilder: (context, child, animation) => FadeTransition(opacity: animation, child: child),
       builder: (context, child) {
@@ -124,9 +125,9 @@ void _onNavigationSelected(TabsRouter router, int index, WidgetRef ref) {
     ref.read(searchInputFocusProvider).requestFocus();
   }
 
-  // Album page
-  if (index == kAlbumTabIndex) {
-    ref.read(remoteAlbumProvider.notifier).refresh();
+  // Spaces page
+  if (index == kSpacesTabIndex) {
+    ref.invalidate(sharedSpacesProvider);
   }
 
   // Library page

alangrainger/immich

mobile/lib/pages/common/tab_shell.page.dart
@@ -11,6 +11,7 @@ import 'package:immich_mobile/generated/translations.g.dart';
 import 'package:immich_mobile/presentation/pages/search/paginated_search.provider.dart';
 import 'package:immich_mobile/providers/haptic_feedback.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
+import 'package:immich_mobile/providers/shared_space.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/readonly_mode.provider.dart';
 import 'package:immich_mobile/providers/search/search_input_focus.provider.dart';
@@ -44,9 +45,9 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
         enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
-        label: context.t.albums,
-        icon: const Icon(Icons.photo_album_outlined),
-        selectedIcon: Icon(Icons.photo_album_rounded, color: context.primaryColor),
+        label: 'Spaces',
+        icon: const Icon(Icons.workspaces_outlined),
+        selectedIcon: Icon(Icons.workspaces, color: context.primaryColor),
         enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
@@ -77,7 +78,7 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
     }
 
     return AutoTabsRouter(
-      routes: const [MainTimelineRoute(), SearchRoute(), AlbumsRoute(), LibraryRoute()],
+      routes: const [MainTimelineRoute(), SearchRoute(), SpacesRoute(), LibraryRoute()],
       duration: const Duration(milliseconds: 600),
       transitionBuilder: (context, child, animation) => FadeTransition(opacity: animation, child: child),
       builder: (context, child) {
@@ -123,9 +124,9 @@ void _onNavigationSelected(TabsRouter router, int index, WidgetRef ref) {
     ref.read(searchInputFocusProvider).requestFocus();
   }
 
-  // Album page
-  if (index == kAlbumTabIndex) {
-    unawaited(ref.read(remoteAlbumProvider.notifier).refresh());
+  // Spaces page
+  if (index == kSpacesTabIndex) {
+    ref.invalidate(sharedSpacesProvider);
   }
 
   // Library page