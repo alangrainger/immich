CarWalTech/gallery

feat(mobile): infinite scroll + sort for live search (#654)
Fixes the page-1-only live photos-filter search: paginating notifier + scroll-to-load-more, 800ms debounce, web-parity sort (relevance/newest/oldest), and a decluttered on-timeline filter strip (Clear all pinned, sort moved to the app bar, count dropped). Removes the unused DriftSearchPage; routes 'view similar photos' into live search.

mobile/lib/pages/common/tab_shell.page.dart
@@ -8,14 +8,12 @@ import 'package:immich_mobile/constants/constants.dart';
 import 'package:immich_mobile/domain/models/events.model.dart';
 import 'package:immich_mobile/domain/utils/event_stream.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
-import 'package:immich_mobile/presentation/pages/search/paginated_search.provider.dart';
 import 'package:immich_mobile/providers/haptic_feedback.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
 import 'package:immich_mobile/providers/shared_space.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/readonly_mode.provider.dart';
-import 'package:immich_mobile/providers/search/search_input_focus.provider.dart';
 import 'package:immich_mobile/providers/tab.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 
@@ -38,12 +36,6 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
         label: 'photos'.tr(),
         icon: const Icon(Icons.photo_library_outlined),
         selectedIcon: Icon(Icons.photo_library, color: context.primaryColor),
-      ),
-      NavigationDestination(
-        label: 'search'.tr(),
-        icon: const Icon(Icons.search_rounded),
-        selectedIcon: Icon(Icons.search, color: context.primaryColor),
-        enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
         label: 'Spaces',
@@ -79,7 +71,7 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
     }
 
     return AutoTabsRouter(
-      routes: const [MainTimelineRoute(), DriftSearchRoute(), SpacesRoute(), DriftLibraryRoute()],
+      routes: const [MainTimelineRoute(), SpacesRoute(), DriftLibraryRoute()],
       duration: const Duration(milliseconds: 600),
       transitionBuilder: (context, child, animation) => FadeTransition(opacity: animation, child: child),
       builder: (context, child) {
@@ -114,15 +106,6 @@ void _onNavigationSelected(TabsRouter router, int index, WidgetRef ref) {
 
   if (index == kPhotoTabIndex) {
     ref.invalidate(driftMemoryFutureProvider);
-  }
-
-  if (router.activeIndex != kSearchTabIndex && index == kSearchTabIndex) {
-    ref.read(searchPreFilterProvider.notifier).clear();
-  }
-
-  // On Search page tapped
-  if (router.activeIndex == kSearchTabIndex && index == kSearchTabIndex) {
-    ref.read(searchInputFocusProvider).requestFocus();
   }
 
   // Spaces page

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -1,13 +1,17 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_sheet.widget.dart';
+import 'package:immich_mobile/presentation/widgets/filter_sheet/sort_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart';
 import 'package:immich_mobile/presentation/widgets/photos_filter/filter_subheader.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/photos_filter_search.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
 
@@ -27,19 +31,63 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
       overrides: [timelineServiceProvider.overrideWith((ref) => ref.watch(photosTimelineQueryProvider))],
       child: Stack(
         children: [
-          Timeline(
-            topSliverWidget: const SliverMainAxisGroup(
-              slivers: [
-                PhotosFilterSubheader(),
-                SliverToBoxAdapter(child: DriftMemoryLane()),
-              ],
+          NotificationListener<ScrollUpdateNotification>(
+            onNotification: (n) {
+              final m = n.metrics;
+              if (m.axis != Axis.vertical) return false;
+              final isSheet = n.context?.findAncestorWidgetOfExactType<DraggableScrollableSheet>() != null;
+              if (!isSheet && m.maxScrollExtent - m.pixels < m.viewportDimension) {
+                ref.read(photosFilterSearchProvider.notifier).loadMore();
+              }
+              return false;
+            },
+            child: Timeline(
+              topSliverWidget: const SliverMainAxisGroup(
+                slivers: [
+                  PhotosFilterSubheader(),
+                  SliverToBoxAdapter(child: DriftMemoryLane()),
+                ],
+              ),
+              topSliverWidgetHeight: hasMemories ? 200 : 0,
+              showStorageIndicator: true,
+              appBar: const ImmichSliverAppBar(
+                floating: true,
+                pinned: false,
+                snap: false,
+                actions: [SortIconButton(), FilterIconButton()],
+              ),
+              bottomSliverWidget: const _SearchLoadMoreFooter(),
             ),
-            topSliverWidgetHeight: hasMemories ? 200 : 0,
-            showStorageIndicator: true,
-            appBar: const ImmichSliverAppBar(floating: true, pinned: false, snap: false, actions: [FilterIconButton()]),
           ),
           const FilterSheet(),
         ],
+      ),
+    );
+  }
+}
+
+class _SearchLoadMoreFooter extends ConsumerWidget {
+  const _SearchLoadMoreFooter();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isActive = ref.watch(photosFilterProvider.select((f) => !f.isEmpty));
+    if (!isActive) return const SliverToBoxAdapter(child: SizedBox.shrink());
+    final isLoading = ref.watch(photosFilterSearchProvider.select((s) => s.isLoading));
+    if (isLoading) {
+      return const SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Center(child: CircularProgressIndicator()),
+        ),
+      );
+    }
+    final done = ref.watch(photosFilterSearchProvider.select((s) => s.nextPage == null));
+    if (!done) return const SliverToBoxAdapter(child: SizedBox.shrink());
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Center(child: Text('search_no_more_result'.tr())),
       ),
     );
   }

alangrainger/immich

mobile/lib/pages/common/tab_shell.page.dart
@@ -8,13 +8,11 @@ import 'package:immich_mobile/domain/models/events.model.dart';
 import 'package:immich_mobile/domain/utils/event_stream.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/generated/translations.g.dart';
-import 'package:immich_mobile/presentation/pages/search/paginated_search.provider.dart';
 import 'package:immich_mobile/providers/haptic_feedback.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
 import 'package:immich_mobile/providers/shared_space.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/readonly_mode.provider.dart';
-import 'package:immich_mobile/providers/search/search_input_focus.provider.dart';
 import 'package:immich_mobile/providers/tab.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 
@@ -37,12 +35,6 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
         label: context.t.photos,
         icon: const Icon(Icons.photo_library_outlined),
         selectedIcon: Icon(Icons.photo_library, color: context.primaryColor),
-      ),
-      NavigationDestination(
-        label: context.t.search,
-        icon: const Icon(Icons.search_rounded),
-        selectedIcon: Icon(Icons.search, color: context.primaryColor),
-        enabled: !isReadonlyModeEnabled,
       ),
       NavigationDestination(
         label: 'Spaces',
@@ -78,7 +70,7 @@ class _TabShellPageState extends ConsumerState<TabShellPage> {
     }
 
     return AutoTabsRouter(
-      routes: const [MainTimelineRoute(), SearchRoute(), SpacesRoute(), LibraryRoute()],
+      routes: const [MainTimelineRoute(), SpacesRoute(), LibraryRoute()],
       duration: const Duration(milliseconds: 600),
       transitionBuilder: (context, child, animation) => FadeTransition(opacity: animation, child: child),
       builder: (context, child) {
@@ -113,15 +105,6 @@ void _onNavigationSelected(TabsRouter router, int index, WidgetRef ref) {
 
   if (index == kPhotoTabIndex) {
     ref.invalidate(memoryLaneProvider);
-  }
-
-  if (router.activeIndex != kSearchTabIndex && index == kSearchTabIndex) {
-    ref.read(searchPreFilterProvider.notifier).clear();
-  }
-
-  // On Search page tapped
-  if (router.activeIndex == kSearchTabIndex && index == kSearchTabIndex) {
-    ref.read(searchInputFocusProvider).requestFocus();
   }
 
   // Spaces page

mobile/lib/presentation/pages/dev/main_timeline.page.dart
@@ -1,15 +1,19 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/presentation/widgets/feature_message/feature_message_dialog.widget.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/filter_sheet/filter_sheet.widget.dart';
+import 'package:immich_mobile/presentation/widgets/filter_sheet/sort_icon_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart';
 import 'package:immich_mobile/presentation/widgets/photos_filter/filter_subheader.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
 import 'package:immich_mobile/providers/feature_message.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/photos_filter.provider.dart';
+import 'package:immich_mobile/providers/photos_filter/photos_filter_search.provider.dart';
 import 'package:immich_mobile/providers/photos_filter/timeline_query.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_sliver_app_bar.dart';
 
@@ -50,19 +54,63 @@ class _MainTimelinePageState extends ConsumerState<MainTimelinePage> {
       overrides: [timelineServiceProvider.overrideWith((ref) => ref.watch(photosTimelineQueryProvider))],
       child: Stack(
         children: [
-          Timeline(
-            topSliverWidget: const SliverMainAxisGroup(
-              slivers: [
-                PhotosFilterSubheader(),
-                SliverToBoxAdapter(child: MemoryLane()),
-              ],
+          NotificationListener<ScrollUpdateNotification>(
+            onNotification: (n) {
+              final m = n.metrics;
+              if (m.axis != Axis.vertical) return false;
+              final isSheet = n.context?.findAncestorWidgetOfExactType<DraggableScrollableSheet>() != null;
+              if (!isSheet && m.maxScrollExtent - m.pixels < m.viewportDimension) {
+                ref.read(photosFilterSearchProvider.notifier).loadMore();
+              }
+              return false;
+            },
+            child: Timeline(
+              topSliverWidget: const SliverMainAxisGroup(
+                slivers: [
+                  PhotosFilterSubheader(),
+                  SliverToBoxAdapter(child: MemoryLane()),
+                ],
+              ),
+              topSliverWidgetHeight: hasMemories ? 200 : 0,
+              showStorageIndicator: true,
+              appBar: const ImmichSliverAppBar(
+                floating: true,
+                pinned: false,
+                snap: false,
+                actions: [SortIconButton(), FilterIconButton()],
+              ),
+              bottomSliverWidget: const _SearchLoadMoreFooter(),
             ),
-            topSliverWidgetHeight: hasMemories ? 200 : 0,
-            showStorageIndicator: true,
-            appBar: const ImmichSliverAppBar(floating: true, pinned: false, snap: false, actions: [FilterIconButton()]),
           ),
           const FilterSheet(),
         ],
+      ),
+    );
+  }
+}
+
+class _SearchLoadMoreFooter extends ConsumerWidget {
+  const _SearchLoadMoreFooter();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isActive = ref.watch(photosFilterProvider.select((f) => !f.isEmpty));
+    if (!isActive) return const SliverToBoxAdapter(child: SizedBox.shrink());
+    final isLoading = ref.watch(photosFilterSearchProvider.select((s) => s.isLoading));
+    if (isLoading) {
+      return const SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Center(child: CircularProgressIndicator()),
+        ),
+      );
+    }
+    final done = ref.watch(photosFilterSearchProvider.select((s) => s.nextPage == null));
+    if (!done) return const SliverToBoxAdapter(child: SizedBox.shrink());
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Center(child: Text('search_no_more_result'.tr())),
       ),
     );
   }