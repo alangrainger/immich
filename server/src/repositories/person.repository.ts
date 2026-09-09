inhotel-io/inreveal

fix(spaces): resolve face recognition race conditions in spaces (#331) (#336)
* fix(spaces): remove thumbnailPath filter from getPersonsBySpaceId

The query filtered on person.thumbnailPath via LEFT JOIN through
representativeFaceId -> asset_face.personId -> person. If the global
person had no thumbnail yet, the space person was silently hidden.
This caused space views to show far fewer people than the global
Photos view (e.g. 21 vs 159 in #331).

Added medium tests covering: no thumbnail, no personId on face,
soft-deleted representative face, and global name fallback.

* fix(spaces): queue space face matching for already-recognized faces

handleRecognizeFaces returned early when face.personId was set,
skipping the space face matching code. Now queues
SharedSpaceFaceMatch before the early return so faces in spaces
created after recognition still get space-matched.

* fix(spaces): queue SharedSpaceFaceMatchAll after recognition jobs

SharedSpaceFaceMatchAll was queued before FacialRecognition jobs on
the same FIFO queue. It ran first when all ML personIds were still
NULL, wasting time scanning ML faces. Moved to after recognition
jobs so it runs last and catches EXIF/manual faces efficiently.

* test(spaces): comprehensive medium tests for face matching pipeline

Tests cover: face matching ordering scenarios (partial recognition,
force reset, idempotency, multi-space), dedup correctness (merge
preservation, missing embeddings, name transfer), recount accuracy
(distinct assets, invisible faces, trashed assets, incremental),
and filtering/phantom photos (assignment tracking, reassignment,
orphan cleanup).

* fix: lint and prettier formatting for CI

* fix(spaces): maintain representativeFaceId during dedup and check library link during sync

Dedup merge now refreshes the target's representativeFaceId after
reassigning faces, preventing stale references when the original
representative face is later deleted. Added repairOrphanedRepresentativeFaces
to restore persons tha

server/src/repositories/person.repository.ts
@@ -206,14 +206,21 @@ export class PersonRepository {
 
   @GenerateSql()
   getAllWithoutFaces() {
+    // The deletedAt / isVisible predicates must live inside the JOIN ON clause,
+    // not in WHERE. A WHERE filter on a LEFT JOIN'd table silently converts it
+    // to an INNER JOIN, which excludes persons with zero asset_face rows entirely
+    // and leaves named zombies uncleaned after a force-recognition reset.
     return this.db
       .selectFrom('person')
       .selectAll('person')
-      .leftJoin('asset_face', 'asset_face.personId', 'person.id')
-      .where('asset_face.deletedAt', 'is', null)
-      .where('asset_face.isVisible', 'is', true)
-      .having((eb) => eb.fn.count('asset_face.assetId'), '=', 0)
+      .leftJoin('asset_face', (join) =>
+        join
+          .onRef('asset_face.personId', '=', 'person.id')
+          .on('asset_face.deletedAt', 'is', null)
+          .on('asset_face.isVisible', 'is', true),
+      )
       .groupBy('person.id')
+      .having((eb) => eb.fn.count('asset_face.assetId'), '=', 0)
       .execute();
   }
 

alangrainger/immich

server/src/repositories/person.repository.ts
@@ -298,14 +298,23 @@ export class PersonRepository {
 
   @GenerateSql()
   getAllWithoutFaces() {
+    // The deletedAt / isVisible predicates must live inside the JOIN ON clause,
+    // not in WHERE. A WHERE filter on a LEFT JOIN'd table silently converts it
+    // to an INNER JOIN, which excludes persons with zero asset_face rows entirely
+    // and leaves named zombies uncleaned after a force-recognition reset.
     return this.db
       .selectFrom('person')
       .selectAll('person')
-      .leftJoin('asset_face', 'asset_face.personGroupId', 'person.personGroupId')
-      .where('asset_face.deletedAt', 'is', null)
-      .where((eb) => eb.or([eb('asset_face.isVisible', 'is', null), eb('asset_face.isVisible', '=', true)]))
-      .having((eb) => eb.fn.count('asset_face.assetId'), '=', 0)
+      .leftJoin('asset_face', (join) =>
+        join
+          .onRef('asset_face.personGroupId', '=', 'person.personGroupId')
+          .on('asset_face.deletedAt', 'is', null)
+          .on((eb) =>
+            eb.or([eb('asset_face.isVisible', 'is', null), eb('asset_face.isVisible', '=', true)]),
+          ),
+      )
       .groupBy(['person.ownerId', 'person.personGroupId'])
+      .having((eb) => eb.fn.count('asset_face.assetId'), '=', 0)
       .execute();
   }
 