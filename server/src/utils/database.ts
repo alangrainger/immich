inhotel-io/inreveal

feat: add filterable timeline with FilterPanel in Spaces (#138)
* docs: add Discovery page design with interactive mockups

Design for a new /discover route that replaces the main timeline with
faceted discovery navigation: search, filtering (people, location,
camera, tags, rating, media type), sorting (capture date, upload date,
filename), and hierarchical temporal picker (year→month).

The discovery filter panel sits as a new collapsible component between
the existing nav sidebar (untouched) and the photo grid. No changes
to upstream layout files — avoids fork conflicts.

Includes six interactive HTML mockups exploring layout options,
filter panel states, and temporal navigation.

* docs: add Discovery page implementation plan (16 tasks)

Detailed TDD implementation plan covering server-side sorting
(AssetSortBy enum, dynamic orderBy), web Discovery panel with
temporal picker, filter sections (people, location, camera, tags,
rating, media type), sort dropdown, active filter chips, collapsed
state with badge indicators, and full E2E test coverage.

Cross-referenced with mockups in docs/plans/mockups/.

* docs: rewrite design for filterable timeline with generic FilterPanel

Pivoted from dedicated /discover page to a reusable FilterPanel
component. V1 ships inside Spaces (fork-only, zero upstream conflict).
Same component later reusable in albums, person views, and main
timeline.

Key changes from previous design:
- No new route — filter panel lives inside existing views
- Extends timeline endpoint with EXIF filters (Option 1 from
  endpoint comparison) instead of using search endpoint
- Search and filtering cleanly separated: filtering is exhaustive
  (timeline + WHERE clauses), search is ranked (CLIP, stays in
  global search bar)
- Person filter is single-select (server constraint)
- Generic component interface with config + data providers
- Comprehensive E2E test coverage (40+ test cases)

Also adds endpoint comparison analysis document.

* docs: update design -

server/src/utils/database.ts
@@ -171,6 +171,21 @@ export function hasPeople<O>(qb: SelectQueryBuilder<DB, 'asset', O>, personIds: 
   );
 }
 
+export function hasAnyPerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, personIds: string[]) {
+  return qb.innerJoin(
+    (eb) =>
+      eb
+        .selectFrom('asset_face')
+        .select('assetId')
+        .where('personId', '=', anyUuid(personIds))
+        .where('deletedAt', 'is', null)
+        .where('isVisible', 'is', true)
+        .groupBy('assetId')
+        .as('has_any_person'),
+    (join) => join.onRef('has_any_person.assetId', '=', 'asset.id'),
+  );
+}
+
 export function hasSpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spacePersonId: string) {
   return qb.where((eb) =>
     eb.exists(
@@ -179,6 +194,18 @@ export function hasSpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spaceP
         .innerJoin('asset_face', 'asset_face.id', 'shared_space_person_face.assetFaceId')
         .whereRef('asset_face.assetId', '=', 'asset.id')
         .where('shared_space_person_face.personId', '=', asUuid(spacePersonId)),
+    ),
+  );
+}
+
+export function hasAnySpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spacePersonIds: string[]) {
+  return qb.where((eb) =>
+    eb.exists(
+      eb
+        .selectFrom('shared_space_person_face')
+        .innerJoin('asset_face', 'asset_face.id', 'shared_space_person_face.assetFaceId')
+        .whereRef('asset_face.assetId', '=', 'asset.id')
+        .where('shared_space_person_face.personId', '=', anyUuid(spacePersonIds)),
     ),
   );
 }
@@ -246,6 +273,18 @@ export function withTagId<O>(qb: SelectQueryBuilder<DB, 'asset', O>, tagId: stri
         .innerJoin('tag_asset', 'tag_asset.tagId', 'tag_closure.id_descendant')
         .whereRef('tag_asset.assetId', '=', 'asset.id')
         .where('tag_closure.id_ancestor', '=', tagId),
+    ),
+  );
+}
+
+export function withAnyTagId<O>(qb: SelectQueryBuilder<DB, 'asset', O>, tagIds: string[]) {
+  return qb.where((eb) =>
+    eb.exists(
+      eb
+        .selectFrom('tag_closure')
+        .innerJoin('tag_asset', 'tag_asset.tagId', 'tag_closure.id_descendant')
+        .whereRef('tag_asset.assetId', '=', 'asset.id')
+        .where('tag_closure.id_ancestor', '=', anyUuid(tagIds)),
     ),
   );
 }

alangrainger/immich

server/src/utils/database.ts
@@ -302,6 +302,21 @@ export function inSharedAlbum(eb: ExpressionBuilder<DB, 'asset'>, userId: string
   );
 }
 
+export function hasAnyPerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, personIds: string[]) {
+  return qb.innerJoin(
+    (eb) =>
+      eb
+        .selectFrom('asset_face')
+        .select('assetId')
+        .where('personGroupId', '=', anyUuid(personIds))
+        .where('deletedAt', 'is', null)
+        .where('isVisible', 'is', true)
+        .groupBy('assetId')
+        .as('has_any_person'),
+    (join) => join.onRef('has_any_person.assetId', '=', 'asset.id'),
+  );
+}
+
 export function hasSpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spacePersonId: string) {
   return qb.where((eb) =>
     eb.exists(
@@ -310,6 +325,18 @@ export function hasSpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spaceP
         .innerJoin('asset_face', 'asset_face.id', 'shared_space_person_face.assetFaceId')
         .whereRef('asset_face.assetId', '=', 'asset.id')
         .where('shared_space_person_face.personId', '=', asUuid(spacePersonId)),
+    ),
+  );
+}
+
+export function hasAnySpacePerson<O>(qb: SelectQueryBuilder<DB, 'asset', O>, spacePersonIds: string[]) {
+  return qb.where((eb) =>
+    eb.exists(
+      eb
+        .selectFrom('shared_space_person_face')
+        .innerJoin('asset_face', 'asset_face.id', 'shared_space_person_face.assetFaceId')
+        .whereRef('asset_face.assetId', '=', 'asset.id')
+        .where('shared_space_person_face.personId', '=', anyUuid(spacePersonIds)),
     ),
   );
 }
@@ -377,6 +404,18 @@ export function withTagId<O>(qb: SelectQueryBuilder<DB, 'asset', O>, tagId: stri
         .innerJoin('tag_asset', 'tag_asset.tagId', 'tag_closure.id_descendant')
         .whereRef('tag_asset.assetId', '=', 'asset.id')
         .where('tag_closure.id_ancestor', '=', tagId),
+    ),
+  );
+}
+
+export function withAnyTagId<O>(qb: SelectQueryBuilder<DB, 'asset', O>, tagIds: string[]) {
+  return qb.where((eb) =>
+    eb.exists(
+      eb
+        .selectFrom('tag_closure')
+        .innerJoin('tag_asset', 'tag_asset.tagId', 'tag_closure.id_descendant')
+        .whereRef('tag_asset.assetId', '=', 'asset.id')
+        .where('tag_closure.id_ancestor', '=', anyUuid(tagIds)),
     ),
   );
 }