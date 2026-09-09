open-noodle/gallery

fix(memories): localize every generated memory card title (#1045) (#1053)
#1012 localized one rule. The other eleven still baked English prose into
the stored title/subtitle at generation time, so "September 2025", "Autumn
2025" and "Your trip to Munich, Germany" reached every viewer in English
whatever their app language. That last one comes from `trip_anniversary` —
a different rule from the `recent_trip` one that was fixed, with nearly
identical wording, which is why it read as a regression.

A memory is generated once and then read for days by clients in whatever
language each viewer picked, so prose written at generation time is stuck.
Every rule now stores only structured facts in `data.context`, and the
clients assemble the card text per render:

- server: no rule emits title/subtitle. `birthday`, `people_together` and
  `person_throwback` now carry the names and month/year they were
  formatting inline, `birthday` a `variant` for its two subtitles, and
  `trip_anniversary` a `yearsAgo`. The English-only helpers those titles
  needed — `monthName`, `SEASON_LABEL`, `Theme.label`, `describeYears` —
  are gone, and `theme.catalog`/`season.util` specs now assert every key
  they emit has a message, so a new theme or season can't silently render
  as "Unknown".
- web: `$lib/utils/memory-card.ts` builds title and subtitle for all
  twelve rules, formatting month names through `Intl` in the viewer's date
  locale rather than needing twelve translated strings.
- mobile: `lib/utils/memory_card_text.dart` does the same for titles and
  replaces two divergent copies of `getMemoryTitle`. Neither had learned
  the `recent_trip` context from #1012, so those cards had been falling
  back to the generic "Memory" label since then.

Cards generated before this change keep their stored title and subtitle,
so none of them regress to "Unknown"; they stay English until retention
cycles them out.

Adds 31 keys to `en.json` and the nine maintained locales, and moves
`recent_trip_tit

mobile/test/presentation/memory/memory_title_test.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
-import 'package:immich_mobile/presentation/pages/drift_memory.page.dart' as drift_page;
-import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart' as memory_lane;
+import 'package:immich_mobile/utils/memory_card_text.dart';
 
+/// Covers the widget-bound wrapper only — the title rules themselves are exercised without a
+/// widget tree in `test/utils/memory_card_text_test.dart`.
 void main() {
-  testWidgets('prefers server title and falls back gracefully when year is absent', (tester) async {
+  DriftMemory memoryWith(Map<String, dynamic> data) => DriftMemory(
+    id: 'memory-rule-1',
+    createdAt: DateTime(2026, 4, 23),
+    updatedAt: DateTime(2026, 4, 23),
+    ownerId: 'user-1',
+    type: MemoryTypeEnum.rule,
+    data: MemoryData(data),
+    isSaved: false,
+    memoryAt: DateTime(2026, 4, 23),
+    showAt: DateTime(2026, 4, 23),
+    hideAt: DateTime(2026, 4, 23, 23, 59),
+    assets: const [],
+  );
+
+  testWidgets('resolves a title through the build context', (tester) async {
     late BuildContext context;
     await tester.pumpWidget(
       MaterialApp(
@@ -18,38 +33,14 @@ void main() {
       ),
     );
 
-    final ruleMemory = DriftMemory(
-      id: 'memory-rule-1',
-      createdAt: DateTime(2026, 4, 23),
-      updatedAt: DateTime(2026, 4, 23),
-      ownerId: 'user-1',
-      type: MemoryTypeEnum.rule,
-      data: const MemoryData({
-        'ruleId': 'birthday',
-        'title': 'Happy birthday, Alice',
-      }),
-      isSaved: false,
-      memoryAt: DateTime(2026, 4, 23),
-      showAt: DateTime(2026, 4, 23),
-      hideAt: DateTime(2026, 4, 23, 23, 59),
-      assets: const [],
+    expect(
+      getMemoryTitle(context, memoryWith({'ruleId': 'birthday', 'title': 'Happy birthday, Alice'})),
+      'Happy birthday, Alice',
     );
 
-    final unknownRuleMemory = DriftMemory(
-      id: 'memory-rule-2',
-      createdAt: DateTime(2026, 4, 23),
-      updatedAt: DateTime(2026, 4, 23),
-      ownerId: 'user-1',
-      type: MemoryTypeEnum.rule,
-      data: const MemoryData({'ruleId': 'recent_trip'}),
-      isSaved: false,
-      memoryAt: DateTime(2026, 4, 23),
-      showAt: DateTime(2026, 4, 23),
-      hideAt: DateTime(2026, 4, 23, 23, 59),
-      assets: const [],
-    );
-
-    expect(memory_lane.getMemoryTitle(context, ruleMemory), 'Happy birthday, Alice');
-    expect(drift_page.getMemoryTitle(context, unknownRuleMemory), isNotEmpty);
+    // No easy_localization in this tree, so translation falls back to the key itself; what
+    // matters here is that a rule with no usable context reaches the generic label rather than
+    // rendering an empty string.
+    expect(getMemoryTitle(context, memoryWith({'ruleId': 'recent_trip'})), 'memory');
   });
 }

mobile/test/utils/memory_card_text_test.dart
@@ -1,0 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:immich_mobile/domain/models/memory.model.dart';
+import 'package:immich_mobile/utils/memory_card_text.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/message_format.dart';
+
+/// Formats the *real* shared `en.json` message, so a key this file names but `en.json` does not
+/// have — or a placeholder no memory rule actually stores — fails here instead of reaching a
+/// user as a raw key.
+final Map<String, dynamic> _en = json.decode(File('../i18n/en.json').readAsStringSync()) as Map<String, dynamic>;
+
+String _translate(String key, {Map<String, Object>? args}) {
+  final message = _en[key];
+  if (message is! String) {
+    throw StateError('en.json has no key "$key"');
+  }
+  return MessageFormat(message, locale: 'en').format(args ?? const {});
+}
+
+DriftMemory _ruleMemory(String ruleId, Map<String, dynamic> context, {String? title}) => DriftMemory(
+  id: 'memory-id',
+  createdAt: DateTime.utc(2026, 4, 23),
+  updatedAt: DateTime.utc(2026, 4, 23),
+  ownerId: 'owner-id',
+  type: MemoryTypeEnum.rule,
+  data: MemoryData({'ruleId': ruleId, 'context': context, if (title != null) 'title': title}),
+  isSaved: false,
+  memoryAt: DateTime.utc(2026, 4, 23),
+  assets: const [],
+);
+
+final _now = DateTime.utc(2026, 4, 23);
+
+String _titleOf(String ruleId, Map<String, dynamic> context) =>
+    buildMemoryTitle(_ruleMemory(ruleId, context), translate: _translate, locale: 'en', now: _now);
+
+void main() {
+  // main.dart does the same before the app renders; DateFormat.yMMMM(locale) throws without it.
+  setUpAll(initializeDateFormatting);
+
+  group('buildMemoryTitle', () {
+    test('prefers a title the server baked in, so memories generated before this change still read', () {
+      final memory = _ruleMemory('month_recap', {'year': 2025, 'month': 9}, title: 'September 2025');
+      expect(buildMemoryTitle(memory, translate: _translate, locale: 'en', now: _now), 'September 2025');
+    });
+
+    test('localizes an on-this-day memory from its year', () {
+      final memory = DriftMemory(
+        id: 'memory-id',
+        createdAt: DateTime.utc(2026, 4, 23),
+        updatedAt: DateTime.utc(2026, 4, 23),
+        ownerId: 'owner-id',
+        type: MemoryTypeEnum.onThisDay,
+        data: const MemoryData({'year': 2024}),
+        isSaved: false,
+        memoryAt: DateTime.utc(2026, 4, 23),
+        assets: const [],
+      );
+      expect(buildMemoryTitle(memory, translate: _translate, locale: 'en', now: _now), '2 years ago');
+    });
+
+    test('builds a month recap title as a locale-formatted month and year', () {
+      expect(_titleOf('month_recap', {'year': 2025, 'month': 9, 'count': 12}), 'September 2025');
+    });
+
+    test('builds a season recap title from the season key, not a baked English label', () {
+      expect(_titleOf('season_recap', {'seasonYear': 2025, 'season': 'autumn', 'count': 20}), 'Autumn 2025');
+    });
+
+    test('builds a trip anniversary title from the place label', () {
+      expect(
+        _titleOf('trip_anniversary', {'placeLabel': 'Munich, Germany', 'yearsAgo': 2}),
+        'Your trip to Munich, Germany',
+      );
+    });
+
+    test('builds a recent trip title from the place label', () {
+      expect(_titleOf('recent_trip', {'placeLabel': 'Paris, France'}), 'Recent trip to Paris, France');
+    });
+
+    test('builds a birthday title from the person name', () {
+      expect(_titleOf('birthday', {'personName': 'Alice', 'variant': 'across_years'}), 'Happy birthday, Alice');
+    });
+
+    test('builds a favorites throwback title from the month and year', () {
+      expect(
+        _titleOf('favorites_throwback', {'year': 2023, 'month': 7, 'count': 6}),
+        'Favorite moments from July 2023',
+      );
+    });
+
+    test('builds an on-this-day-in-a-place title from the city', () {
+      expect(
+        _titleOf('on_this_day_place', {
+          'city': 'Lisbon',
+          'count': 11,
+          'years': [2021, 2023],
+        }),
+        'On this day in Lisbon',
+      );
+    });
+
+    test('builds a people-together title from both names', () {
+      expect(_titleOf('people_together', {'personAName': 'Anna', 'personBName': 'Ben', 'count': 6}), 'Anna & Ben');
+    });
+
+    test('builds a video moments title from the month and year', () {
+      expect(_titleOf('video_moments', {'year': 2023, 'month': 7, 'count': 9}), 'Video moments from July 2023');
+    });
+
+    test('builds a themed title from the theme key, not a baked English label', () {
+      expect(_titleOf('themed', {'year': 2023, 'theme': 'sunset', 'count': 18}), 'Sunsets from 2023');
+    });
+
+    test('builds a person throwback title from the person name', () {
+      expect(
+        _titleOf('person_throwback', {'personName': 'Anna', 'count': 23, 'month': 8, 'year': 2023}),
+        'Times with Anna',
+      );
+    });
+
+    test('falls back to the generic memory label for a rule it has no title for', () {
+      expect(_titleOf('some_future_rule', const {}), 'Memory');
+    });
+
+    test('falls back to the generic memory label when the context is missing the fields it needs', () {
+      expect(_titleOf('season_recap', const {'count': 20}), 'Memory');
+    });
+  });
+}

server/src/services/memory-rules/birthday.rule.ts
@@ -43,12 +43,15 @@ export class BirthdayMemoryRule implements MemoryRule {
         candidates.push({
           ruleId: this.id,
           dedupeKey: `birthday:${person.id}:${target.toFormat('yyyy-MM-dd')}`,
-          title: `Happy birthday, ${person.name}`,
-          subtitle: 'Photos from different years',
           score: 300 + byYear.size * 10 + assetIds.length,
           assetIds,
           memoryAt: target,
-          context: { personId: person.id, distinctYears: byYear.size },
+          context: {
+            personId: person.id,
+            personName: person.name,
+            variant: 'across_years',
+            distinctYears: byYear.size,
+          },
         });
 
         continue;
@@ -66,12 +69,10 @@ export class BirthdayMemoryRule implements MemoryRule {
       candidates.push({
         ruleId: this.id,
         dedupeKey: `birthday:${person.id}:${target.toFormat('yyyy-MM-dd')}`,
-        title: `Happy birthday, ${person.name}`,
-        subtitle: `Recent photos of ${person.name}`,
         score: 250 + fallbackAssetIds.length,
         assetIds: fallbackAssetIds,
         memoryAt: target,
-        context: { personId: person.id, distinctYears: byYear.size },
+        context: { personId: person.id, personName: person.name, variant: 'recent', distinctYears: byYear.size },
       });
     }
 

alangrainger/immich

mobile/test/presentation/memory/memory_title_test.dart
@@ -1,55 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
-import 'package:immich_mobile/presentation/pages/memory.page.dart' as drift_page;
-import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart' as memory_lane;
-import '../../widget_tester_extensions.dart';
+import 'package:immich_mobile/utils/memory_card_text.dart';
 
+/// Covers the widget-bound wrapper only — the title rules themselves are exercised without a
+/// widget tree in `test/utils/memory_card_text_test.dart`.
 void main() {
-  testWidgets('prefers server title and falls back gracefully when year is absent', (tester) async {
+  Memory memoryWith(Map<String, dynamic> data) => Memory(
+    id: 'memory-rule-1',
+    createdAt: DateTime(2026, 4, 23),
+    updatedAt: DateTime(2026, 4, 23),
+    ownerId: 'user-1',
+    type: MemoryTypeEnum.rule,
+    data: MemoryData(data),
+    isSaved: false,
+    memoryAt: DateTime(2026, 4, 23),
+    showAt: DateTime(2026, 4, 23),
+    hideAt: DateTime(2026, 4, 23, 23, 59),
+    assets: const [],
+  );
+
+  testWidgets('resolves a title through the build context', (tester) async {
     late BuildContext context;
     await tester.pumpWidget(
-      localizedForTest(
-        MaterialApp(
-          home: Builder(
-            builder: (ctx) {
-              context = ctx;
-              return const SizedBox.shrink();
-            },
-          ),
+      MaterialApp(
+        home: Builder(
+          builder: (ctx) {
+            context = ctx;
+            return const SizedBox.shrink();
+          },
         ),
       ),
     );
 
-    final ruleMemory = Memory(
-      id: 'memory-rule-1',
-      createdAt: DateTime(2026, 4, 23),
-      updatedAt: DateTime(2026, 4, 23),
-      ownerId: 'user-1',
-      type: MemoryTypeEnum.rule,
-      data: const MemoryData({'ruleId': 'birthday', 'title': 'Happy birthday, Alice'}),
-      isSaved: false,
-      memoryAt: DateTime(2026, 4, 23),
-      showAt: DateTime(2026, 4, 23),
-      hideAt: DateTime(2026, 4, 23, 23, 59),
-      assets: const [],
+    expect(
+      getMemoryTitle(context, memoryWith({'ruleId': 'birthday', 'title': 'Happy birthday, Alice'})),
+      'Happy birthday, Alice',
     );
 
-    final unknownRuleMemory = Memory(
-      id: 'memory-rule-2',
-      createdAt: DateTime(2026, 4, 23),
-      updatedAt: DateTime(2026, 4, 23),
-      ownerId: 'user-1',
-      type: MemoryTypeEnum.rule,
-      data: const MemoryData({'ruleId': 'recent_trip'}),
-      isSaved: false,
-      memoryAt: DateTime(2026, 4, 23),
-      showAt: DateTime(2026, 4, 23),
-      hideAt: DateTime(2026, 4, 23, 23, 59),
-      assets: const [],
-    );
-
-    expect(memory_lane.getMemoryTitle(context, ruleMemory), 'Happy birthday, Alice');
-    expect(drift_page.getMemoryTitle(context, unknownRuleMemory), isNotEmpty);
+    // No easy_localization in this tree, so translation falls back to the key itself; what
+    // matters here is that a rule with no usable context reaches the generic label rather than
+    // rendering an empty string.
+    expect(getMemoryTitle(context, memoryWith({'ruleId': 'recent_trip'})), 'memory');
   });
 }

mobile/test/utils/memory_card_text_test.dart
@@ -1,0 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:immich_mobile/domain/models/memory.model.dart';
+import 'package:immich_mobile/utils/memory_card_text.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/message_format.dart';
+
+/// Formats the *real* shared `en.json` message, so a key this file names but `en.json` does not
+/// have — or a placeholder no memory rule actually stores — fails here instead of reaching a
+/// user as a raw key.
+final Map<String, dynamic> _en = json.decode(File('../i18n/en.json').readAsStringSync()) as Map<String, dynamic>;
+
+String _translate(String key, {Map<String, Object>? args}) {
+  final message = _en[key];
+  if (message is! String) {
+    throw StateError('en.json has no key "$key"');
+  }
+  return MessageFormat(message, locale: 'en').format(args ?? const {});
+}
+
+Memory _ruleMemory(String ruleId, Map<String, dynamic> context, {String? title}) => Memory(
+  id: 'memory-id',
+  createdAt: DateTime.utc(2026, 4, 23),
+  updatedAt: DateTime.utc(2026, 4, 23),
+  ownerId: 'owner-id',
+  type: MemoryTypeEnum.rule,
+  data: MemoryData({'ruleId': ruleId, 'context': context, if (title != null) 'title': title}),
+  isSaved: false,
+  memoryAt: DateTime.utc(2026, 4, 23),
+  assets: const [],
+);
+
+final _now = DateTime.utc(2026, 4, 23);
+
+String _titleOf(String ruleId, Map<String, dynamic> context) =>
+    buildMemoryTitle(_ruleMemory(ruleId, context), translate: _translate, locale: 'en', now: _now);
+
+void main() {
+  // main.dart does the same before the app renders; DateFormat.yMMMM(locale) throws without it.
+  setUpAll(initializeDateFormatting);
+
+  group('buildMemoryTitle', () {
+    test('prefers a title the server baked in, so memories generated before this change still read', () {
+      final memory = _ruleMemory('month_recap', {'year': 2025, 'month': 9}, title: 'September 2025');
+      expect(buildMemoryTitle(memory, translate: _translate, locale: 'en', now: _now), 'September 2025');
+    });
+
+    test('localizes an on-this-day memory from its year', () {
+      final memory = Memory(
+        id: 'memory-id',
+        createdAt: DateTime.utc(2026, 4, 23),
+        updatedAt: DateTime.utc(2026, 4, 23),
+        ownerId: 'owner-id',
+        type: MemoryTypeEnum.onThisDay,
+        data: const MemoryData({'year': 2024}),
+        isSaved: false,
+        memoryAt: DateTime.utc(2026, 4, 23),
+        assets: const [],
+      );
+      expect(buildMemoryTitle(memory, translate: _translate, locale: 'en', now: _now), '2 years ago');
+    });
+
+    test('builds a month recap title as a locale-formatted month and year', () {
+      expect(_titleOf('month_recap', {'year': 2025, 'month': 9, 'count': 12}), 'September 2025');
+    });
+
+    test('builds a season recap title from the season key, not a baked English label', () {
+      expect(_titleOf('season_recap', {'seasonYear': 2025, 'season': 'autumn', 'count': 20}), 'Autumn 2025');
+    });
+
+    test('builds a trip anniversary title from the place label', () {
+      expect(
+        _titleOf('trip_anniversary', {'placeLabel': 'Munich, Germany', 'yearsAgo': 2}),
+        'Your trip to Munich, Germany',
+      );
+    });
+
+    test('builds a recent trip title from the place label', () {
+      expect(_titleOf('recent_trip', {'placeLabel': 'Paris, France'}), 'Recent trip to Paris, France');
+    });
+
+    test('builds a birthday title from the person name', () {
+      expect(_titleOf('birthday', {'personName': 'Alice', 'variant': 'across_years'}), 'Happy birthday, Alice');
+    });
+
+    test('builds a favorites throwback title from the month and year', () {
+      expect(
+        _titleOf('favorites_throwback', {'year': 2023, 'month': 7, 'count': 6}),
+        'Favorite moments from July 2023',
+      );
+    });
+
+    test('builds an on-this-day-in-a-place title from the city', () {
+      expect(
+        _titleOf('on_this_day_place', {
+          'city': 'Lisbon',
+          'count': 11,
+          'years': [2021, 2023],
+        }),
+        'On this day in Lisbon',
+      );
+    });
+
+    test('builds a people-together title from both names', () {
+      expect(_titleOf('people_together', {'personAName': 'Anna', 'personBName': 'Ben', 'count': 6}), 'Anna & Ben');
+    });
+
+    test('builds a video moments title from the month and year', () {
+      expect(_titleOf('video_moments', {'year': 2023, 'month': 7, 'count': 9}), 'Video moments from July 2023');
+    });
+
+    test('builds a themed title from the theme key, not a baked English label', () {
+      expect(_titleOf('themed', {'year': 2023, 'theme': 'sunset', 'count': 18}), 'Sunsets from 2023');
+    });
+
+    test('builds a person throwback title from the person name', () {
+      expect(
+        _titleOf('person_throwback', {'personName': 'Anna', 'count': 23, 'month': 8, 'year': 2023}),
+        'Times with Anna',
+      );
+    });
+
+    test('falls back to the generic memory label for a rule it has no title for', () {
+      expect(_titleOf('some_future_rule', const {}), 'Memory');
+    });
+
+    test('falls back to the generic memory label when the context is missing the fields it needs', () {
+      expect(_titleOf('season_recap', const {'count': 20}), 'Memory');
+    });
+  });
+}

server/src/services/memory-rules/birthday.rule.ts
@@ -43,12 +43,15 @@ export class BirthdayMemoryRule implements MemoryRule {
         candidates.push({
           ruleId: this.id,
           dedupeKey: `birthday:${person.personGroupId}:${target.toFormat('yyyy-MM-dd')}`,
-          title: `Happy birthday, ${person.name}`,
-          subtitle: 'Photos from different years',
           score: 300 + byYear.size * 10 + assetIds.length,
           assetIds,
           memoryAt: target,
-          context: { personId: person.personGroupId, distinctYears: byYear.size },
+          context: {
+            personId: person.personGroupId,
+            personName: person.name,
+            variant: 'across_years',
+            distinctYears: byYear.size,
+          },
         });
 
         continue;
@@ -66,12 +69,10 @@ export class BirthdayMemoryRule implements MemoryRule {
       candidates.push({
         ruleId: this.id,
         dedupeKey: `birthday:${person.personGroupId}:${target.toFormat('yyyy-MM-dd')}`,
-        title: `Happy birthday, ${person.name}`,
-        subtitle: `Recent photos of ${person.name}`,
         score: 250 + fallbackAssetIds.length,
         assetIds: fallbackAssetIds,
         memoryAt: target,
-        context: { personId: person.personGroupId, distinctYears: byYear.size },
+        context: { personId: person.personGroupId, personName: person.name, variant: 'recent', distinctYears: byYear.size },
       });
     }
 